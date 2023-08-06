50f36ce829d7db04471a73ff76039fcfcf2da2e1
Peet Terluin <peet_terluin@terluin.nl>
2023-08-06 15:26:57 +0200

    Visual Studio naar eigen repo

---
+++ Learning/pl002/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace pl002
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.halloToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hahaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.tb0001 = new System.Windows.Forms.MaskedTextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.halloToolStripMenuItem,
+            this.hahaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1034, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // halloToolStripMenuItem
+            // 
+            this.halloToolStripMenuItem.Name = "halloToolStripMenuItem";
+            this.halloToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.halloToolStripMenuItem.Text = "Hallo";
+            // 
+            // hahaToolStripMenuItem
+            // 
+            this.hahaToolStripMenuItem.Name = "hahaToolStripMenuItem";
+            this.hahaToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.hahaToolStripMenuItem.Text = "Haha";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(337, 156);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.Click += new System.EventHandler(this.checkBox1_Click);
+            // 
+            // tb0001
+            // 
+            this.tb0001.Location = new System.Drawing.Point(400, 292);
+            this.tb0001.Name = "tb0001";
+            this.tb0001.Size = new System.Drawing.Size(100, 20);
+            this.tb0001.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1034, 611);
+            this.Controls.Add(this.tb0001);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem halloToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hahaToolStripMenuItem;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.MaskedTextBox tb0001;
+    }
+}
+
---
+++ Learning/pl002/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pl002
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked) {
+                tb0001.Text = "Holadiee";
+                    }
+            else {
+                tb0001.Text = "Not checked";
+                    }  
+
+        }
+    }
+}
